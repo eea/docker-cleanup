4188945de50292cc7b29c25b1bc87de8bf8a71f1
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2022-03-29 08:57:05 +0300

    Fix log information

--- run.sh
+++ run.sh
@@ -157,10 +157,10 @@ do
     # Cleanup blocked non-rancher containers
     # If CLEAN_NON_RANCHER is a .  then all containers are kept and there is no need to check.
     if [ "${CLEAN_NON_RANCHER}" != "." ]; then
-        info "Removing blocked containers using 'docker-cleanup-containers.sh' script"
+        info "Removing blocked non-rancher containers using 'docker-cleanup-containers.sh' script"
         /docker-cleanup-containers.sh
     else
-      info "Configured to not clean containers"
+      info "Configured to not clean non-rancher containers"
     fi
 
 
